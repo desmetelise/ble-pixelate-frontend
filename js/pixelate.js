c8adaaf297f60f97593ddea0a2a291e60cdda2c5
Elise De Smet <elise.desmet99@gmail.com>
2019-10-25 16:09:18 +0200

    added color

--- js/pixelate.js
+++ js/pixelate.js
@@ -1,25 +1,45 @@
-let canvas = document.getElementById("displayCanvas");
+let canvas = document.getElementById('displayCanvas');
 let display = new Display(canvas);
 let cursor = new Cursor(display);
 
 let controller = new BleController();
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("connect").addEventListener("click", () => {
-        controller.connect()
-        .then(() => {
-            console.log("Connected to BLE controller");
-            cursor.change_color({r:0, g:0, b:200});
-            controller.addEventListener('touch', (event) => {
-                switch(event.touchedKey) {
-                    case 'R': cursor.move_right(); break;
-                    case 'L': cursor.move_left(); break;
-                    case 'U': cursor.move_up(); break;
-                    case 'D': cursor.move_down(); break;
-                    case 'X': cursor.colorize({r: 200, g:0, b:0}); break;
-                }
-            });
-        }).catch((error) => {
-            console.log(error);
-        });
-    });
+	document.getElementById('connect').addEventListener('click', () => {
+		controller
+			.connect()
+			.then(() => {
+				console.log('Connected to BLE controller');
+				controller.addEventListener('touch', (event) => {
+					switch (event.touchedKey) {
+						case 'R':
+							cursor.move_right();
+							break;
+						case 'L':
+							cursor.move_left();
+							break;
+						case 'U':
+							cursor.move_up();
+							break;
+						case 'D':
+							cursor.move_down();
+							break;
+						case 'X':
+							cursor.change_color({ r: 51, g: 153, b: 255 });
+							cursor.colorize();
+							break;
+						case 'A':
+							cursor.change_color({ r: 255, g: 0, b: 127 });
+							cursor.colorize();
+							break;
+						case 'B':
+							cursor.change_color({ r: 127, g: 0, b: 255 });
+							cursor.colorize();
+							break;
+					}
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	});
 });
